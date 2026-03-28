cdc5e50a364e6fb46b60c9e94a3903e3760612ab
Akshansh <akshanshz@gmail.com>
2026-03-28 09:33:03 +0100

    graph updated with learning rate adaptation

--- tutor/builder.py
+++ tutor/builder.py
@@ -3,12 +3,15 @@ from langgraph.checkpoint.memory import InMemorySaver
 from dotenv import load_dotenv
 
 from tutor.schemas.state import TutorState
-from tutor.services.decision import decide_next_action
-from tutor.services.diagnosis import diagnose
-from tutor.services.evaluation import evaluate_answer
+
+from tutor.services.learning_rate import update_learning_rate
 from tutor.services.question import ask_question
+from tutor.services.evaluation import evaluate_answer
 from tutor.services.mastery import update_mastery
+from tutor.services.diagnosis import diagnose
+from tutor.services.decision import decide_next_action
 from tutor.services.human_loop import human_review
+
 from tutor.routers import route_after_human_review, route_after_decision
 from tutor.tools.application import queue_tool_call, apply_tool_result
 from tutor.tools.support_tools import tool_node
@@ -20,6 +23,7 @@ builder = StateGraph(TutorState)
 
 builder.add_node("load_student", load_student)
 builder.add_node("choose_skill", choose_skill)
+builder.add_node("update_learning_rate", update_learning_rate)
 builder.add_node("ask_question", ask_question)
 builder.add_node("evaluate_answer", evaluate_answer)
 builder.add_node("update_mastery", update_mastery)
@@ -32,8 +36,8 @@ builder.add_node("apply_tool_result", apply_tool_result)
 
 builder.add_edge(START, "load_student")
 builder.add_edge("load_student", "choose_skill")
-builder.add_edge("choose_skill", "ask_question")
-
+builder.add_edge("choose_skill", "update_learning_rate")
+builder.add_edge("update_learning_rate", "ask_question")
 builder.add_edge("ask_question", "evaluate_answer")
 builder.add_edge("evaluate_answer", "update_mastery")
 builder.add_edge("update_mastery", "diagnose")
