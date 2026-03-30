6f7060916db2b7c9d39a4af870c3b438d5719a89
Akshansh <akshanshz@gmail.com>
2026-03-30 10:12:14 +0200

    added rag for lesson generation

--- tutor/retrieval/context.py
+++ tutor/retrieval/context.py
@@ -1,8 +1,49 @@
-from tutor.config.rag_settings import answer_rag, question_rag, careless_rag, misconcept_rag, decision_rag
+from tutor.config.rag_settings import answer_rag, question_rag, careless_rag, misconcept_rag, decision_rag, lesson_rag
 from tutor.retrieval.chroma_store import get_vectordb
 from tutor.schemas.state import TutorState
 
 from langsmith import traceable
+
+@traceable
+def lesson_context(state: TutorState) -> str:
+
+    vectordb = get_vectordb(lesson_rag)
+
+    skill = state['current_skill']
+    mastery = state["mastery"][skill]
+    window = 1
+
+    all_docs = vectordb.get(
+        where={"lesson": skill},
+        include=["documents", "metadatas"]
+    )
+
+    pages = sorted({
+        m["page"] for m in all_docs["metadatas"] if m is not None and "page" in m
+    })
+    start_page, end_page = min(pages), max(pages)
+    target_page = round(start_page + (end_page - start_page) * mastery)
+    page_from = max(start_page, (target_page - window))
+    page_to = min(end_page, (target_page + window))
+
+    docs = vectordb.get(
+        where={
+            "$and": [
+                {"lesson": skill},
+                {"page": {"$gte": page_from}},
+                {"page": {"$lte": page_to}}
+            ]
+        },
+        include=["documents", "metadatas"]
+    )
+
+    pairs = sorted(
+        zip(docs["metadatas"], docs["documents"]),
+        key=lambda x: (x[0]["page"], x[0].get("chunk", 0))
+    ) 
+
+    context_prompt = "\n\n".join(doc for _, doc in pairs)
+    return context_prompt
 
 @traceable
 def question_context(state: TutorState) -> str:
